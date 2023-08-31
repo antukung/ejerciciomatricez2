74a4ff290a2abb9a1259fb0c75bc1d8588166afb
Antu <122485403+antukung@users.noreply.github.com>
2023-08-31 12:26:24 -0300

    aca esta todo matrices

---
+++ algoritmo.js
@@ -0,0 +1,34 @@
+const divTablero = document.querySelector("#tablero");
+
+
+const matriz = []
+
+function boton() {
+    let inputFila = parseInt(document.querySelector("#fila").value);
+
+
+    for (let fila = 0; fila < inputFila; fila++) {
+        divTablero.innerHTML += `
+    <div class="row">
+        ${agregarColumna()}
+        </div>`
+    }
+
+
+
+}
+function agregarColumna() {
+    
+
+     let inputColumna = parseInt(document.querySelector("#columna").value);
+
+    let columnas = "";
+
+    for (let columna = 0; columna < inputColumna; columna++) {
+
+        columnas += `<div class="col casilla">
+        </div>`
+
+    }
+    return columnas; 
+}
